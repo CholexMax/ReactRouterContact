b906828e32f69b8f427ea0e2b2e46ce6af34a8bd
CholeXAmber <tobexlifenew@gmail.com>
2025-01-05 15:23:15 +0000

    Postcss componnet creatd

---
+++ ContactProject/src/Components/PostCss.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PostCss() {
+  return (
+    <div>A PostCSS-powered tool for integrating Tailwind CSS into your project. It processes your styles, enables custom configurations, and optimizes your CSS output for production. Perfect for creating scalable and efficient design systems.</div>
+  )
+}
+
+export default PostCss
