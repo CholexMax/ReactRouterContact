760c7facf5fa90b96883c547891d51a04b7f9bda
CholeXAmber <tobexlifenew@gmail.com>
2025-01-05 15:21:59 +0000

    framework creatd

---
+++ ContactProject/src/Components/Framwork.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Framwork() {
+  return (
+    <div>Framwork
+        <br/>
+        react
+        <br/>
+        nextjs
+        <br/>
+        swelte
+        <br/>
+        vue
+        <br/>
+        flask
+        <br/>
+    </div>
+  )
+}
+
+export default Framwork
