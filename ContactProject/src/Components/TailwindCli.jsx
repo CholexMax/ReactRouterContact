0e287f95170c0683bf8c433fcd5f58819b435df5
CholeXAmber <tobexlifenew@gmail.com>
2025-01-05 15:23:43 +0000

    TailwindCli  componnet creatd

---
+++ ContactProject/src/Components/TailwindCli.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function TailwindCli() {
+  return (
+    <div style={
+        {
+            color:"red"
+        }
+    }>
+
+
+A simple CLI tool to streamline your Tailwind CSS workflow. Quickly generate utility classes, preview styles, and optimize Tailwind configurations directly from the terminal. Ideal for developers aiming to enhance productivity in styling web projects.
+
+    </div>
+  )
+}
+
+export default TailwindCli
