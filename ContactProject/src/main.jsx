01d073f1b0197cc03334ce4ba5110cf1e4f46b2e
CholeXAmber <tobexlifenew@gmail.com>
2025-01-05 15:19:07 +0000

    added new routes

--- ContactProject/src/main.jsx
+++ ContactProject/src/main.jsx
@@ -5,16 +5,29 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import Blog from './Components/Blog'
 import ShowCase from './Components/ShowCase'
 import Home from './Components/Home'
-import Docs from './Components/Docs'
+import Layout2 from './Layout/Layout2'
+import TailwindCli from './Components/TailwindCli'
+import PostCss from './Components/PostCss'
+import Framwork from './Components/Framwork'
+import Cdn from './Components/Cdn'
+import Profile from './Components/Profile'
 
 const route=createBrowserRouter(
   createRoutesFromElements(
+    <>
     <Route path='/' element={<Layout/>}>
       <Route path='' element={<Home/>}/>
       <Route path='blog' element={<Blog/>}/>
-      <Route path='docs' element={<Docs/>}/>
+          <Route path='docs' element={<Layout2/>}>
+             <Route path='installation' element={<TailwindCli/>}/>
+             <Route path='postcss' element={<PostCss/>}/>
+             <Route path='framework' element={<Framwork/>}/>
+             <Route path='cdn' element={<Cdn/>}/>
+x         </Route>
       <Route path='showcase' element={<ShowCase/>}/>
     </Route>
+      <Route path='info/:userName' element={<Profile/>}/>
+    </>
   )
 )
 
